1b1d4ecf0d7f1e6825b3299d453ee327f3f2bd91
biji <noel.dovan@epitech.eu>
2016-10-10 00:24:26 +0200

    1.2 added support for google play (still need to test)

--- app/src/main/java/com/dev/biji/nicememewebsite/MainActivity.java
+++ app/src/main/java/com/dev/biji/nicememewebsite/MainActivity.java
@@ -2,23 +2,33 @@ package com.dev.biji.nicememewebsite;
 
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RemoteViews;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
+
 import layout.NewAppWidget;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     private SharedPreferences _sharedPref;
     private SharedPreferences.Editor _editor;
     private int _count;
     private TextView _countText;
+    private GoogleApiClient _googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +48,38 @@ public class MainActivity extends AppCompatActivity {
         // update view
         _countText = (TextView) findViewById(R.id.countText);
         _countText.setText(String.valueOf(_count));
+
+        // google
+        _googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                // add other APIs and scopes here as needed
+                .build();
+        _googleApiClient.connect();
+        System.out.println("Connect");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (resultCode == RESULT_OK) {
+            _googleApiClient.connect();
+            System.out.println("Connect result");
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        _googleApiClient.connect();
+        System.out.println("Connect start");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        _googleApiClient.disconnect();
+        System.out.println("dConnect");
     }
 
     @Override
@@ -91,5 +133,38 @@ public class MainActivity extends AppCompatActivity {
                         }, 2000);
             }
         });
+
+        // achievement
+        if (_googleApiClient != null && _googleApiClient.isConnected()) {
+            System.out.println("check achiev");
+            switch (_count) {
+                case 1:
+                    Games.Achievements.unlock(_googleApiClient, getString(R.string.achievement_1));
+                case 10:
+                    Games.Achievements.unlock(_googleApiClient, getString(R.string.achievement_2));
+                case 100:
+                    Games.Achievements.unlock(_googleApiClient, getString(R.string.achievement_3));
+                case 1000:
+                    Games.Achievements.unlock(_googleApiClient, getString(R.string.achievement_4));
+                case 10000:
+                    Games.Achievements.unlock(_googleApiClient, getString(R.string.achievement_5));
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        _googleApiClient.connect();
+        System.out.println("Connect suspend");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
